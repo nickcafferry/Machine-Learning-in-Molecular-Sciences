6ab389a843e6e1631b662d8cd70ef72c25b27459
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-07-24 18:42:57 +0800

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath("../../mlms/"))
+
+project = 'ml-ms'
+copyright = '- Wei MEI.'
+author = 'nickcafferry-wei_mei'
+
+version = '0.1.0'
+release = ''
+
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
+]
+
+autoclass_content = 'both'
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = 'English'
+html_search_language = 'English'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'remain']
+pygments_style = 'default'
+
+html_static_path = ['assets']
+html_theme = 'sphinx_rtd_theme'
+html_favicon = 'imperfection.png'
+html_logo = 'imperfection.png'
+html_theme_options = {
+    'logo_only': True,
+    'style_nav_header_background': '#343131',
+}
