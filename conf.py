7f87cebe1ec5b4cd89e901cf45acb98d8adbf014
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2020-07-24 19:15:10 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -35,9 +35,9 @@ pygments_style = 'default'
 
 html_static_path = ['assets']
 html_theme = 'sphinx_rtd_theme'
-html_favicon = 'https://s1.ax1x.com/2020/07/21/UosEmd.md.jpg'
-html_logo = 'https://s1.ax1x.com/2020/07/21/UosEmd.md.jpg'
+html_favicon = 'GCC.png'
+html_logo = 'GCC.png'
 html_theme_options = {
-    'logo_only': True,
+    'logo_only': False,
     'style_nav_header_background': '#343131',
 }
