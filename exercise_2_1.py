de285eaec526ca257a616999bd25eba4bbd1b9e3
nickcafferry <56918345+nickcafferry@users.noreply.github.com>
2020-08-16 14:04:18 +0000

    Deploying to master from  @ 80d0f3620be9789e972ac1c82ce5df2f09cc3c62 🚀

---
+++ exercise_2_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import qml
+
+if __name__ == "__main__":
+
+
+    # Create the compound object mol from the file qm7/0001.xyz which happens to be methane
+    mol = qml.Compound(xyz="qm7/0001.xyz")
+
+    # Generate and print a coulomb matrix for compound with 5 atoms 
+    mol.generate_coulomb_matrix(size=5, sorting="row-norm")
+    print(mol.representation)
+
+    # Generate and print BoB bags for compound containing C and H
+    mol.generate_bob(size=5, asize={"C":2, "H":5})
+    print(mol.representation)
+
+    # Print other properties stored in the object
+    print(mol.coordinates)
+    print(mol.atomtypes)
+    print(mol.nuclear_charges)
+    print(mol.name)
+    print(mol.unit_cell)
---
+++ exercise_2_2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import numpy as np
+
+import qml
+from qml.kernels import gaussian_kernel
+from qml.kernels import laplacian_kernel
+
+from tutorial_data import compounds
+
+if __name__ == "__main__":
+
+    # Import QM7, already parsed to QML
+    from tutorial_data import compounds
+
+    from qml.kernels import gaussian_kernel
+    
+    # For every compound generate a coulomb matrix or BoB
+    for mol in compounds:
+
+        mol.generate_coulomb_matrix(size=23, sorting="row-norm")
+        # mol.generate_bob(size=23, asize={"O":3, "C":7, "N":3, "H":16, "S":1})
+
+    # Make a big 2D array with all the representations
+    X = np.array([mol.representation for mol in compounds])
+    # X = np.array([mol.bob for mol in compounds])
+
+    # Print all representations
+    print("Representations:")
+    print(X)
+
+    # Run on only a subset of the first 100 (for speed)
+    X = X[:100]
+
+    # Define the kernel width
+    sigma = 1000.0
+
+    # K is also a Numpy array
+    K = gaussian_kernel(X, X, sigma)
+    
+    # Print the kernel
+    print("Gaussian kernel:")
+    print(K)
